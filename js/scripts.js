b20ba9390e0496b19f8f23d67303ab82e2804559
Ramirezciani <pau.ramirez@duocuc.cl>
2022-11-07 10:49:39 -0300

    PortFolio

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleText = document.getElementById('toggle-text');
+
+toggleTheme.addEventListener("click", ()=>{
+    document.body.classList.toggle("dark");
+    if(toggleIcon.scroll.includes("moon.svg")){
+        toggleIcon.src="img/sun.svg";
+        toggleText.textContent="Light Mode";
+    }else{
+        toggleIcon.src = "img/moon.svg";
+        toggleText.textContent = "Dark Mode"
+
+    }
+})
