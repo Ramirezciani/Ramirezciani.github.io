69ba8982aa6b883e60386e8d623175c8e65edc70
Ramirezciani <pau.ramirez@duocuc.cl>
2022-11-07 11:05:52 -0300

    dark/lightmode

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ const toggleTheme = document.getElementById('toggle-theme');
 const toggleIcon = document.getElementById('toggle-icon');
 const toggleText = document.getElementById('toggle-text');
 
-toggleTheme.addEventListener("click", ()=>{
+toggleTheme.addEventListener("click", ( ) => {
     document.body.classList.toggle("dark");
-    if(toggleIcon.scroll.includes("moon.svg")){
+    if(toggleIcon.src.includes("moon.svg")){
         toggleIcon.src="img/sun.svg";
         toggleText.textContent="Light Mode";
     }else{
         toggleIcon.src = "img/moon.svg";
-        toggleText.textContent = "Dark Mode"
+        toggleText.textContent = "Dark Mode";
 
     }
 })
